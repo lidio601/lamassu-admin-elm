a6003ab2c32ab4338e3aad27b38652a85e66aa75
Josh Harvey <josh@lamassu.is>
2016-12-04 16:11:00 +0200

    WIP

---
+++ src/TransactionDecoder.elm
@@ -0,0 +1,67 @@
+module TransactionDecoder exposing (..)
+
+import Json.Decode exposing (..)
+import Json.Decode.Extra exposing (date)
+import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
+import TransactionTypes exposing (..)
+
+
+txDecode : String -> Decoder Tx
+txDecode txClass =
+    case txClass of
+        "cashIn" ->
+            map CashInTx cashInTxDecoder
+
+        "cashOut" ->
+            map CashOutTx cashOutTxDecoder
+
+        _ ->
+            fail ("Unknown tx class: " ++ txClass)
+
+
+txsDecoder : Decoder (List Tx)
+txsDecoder =
+    (field "transactions" (list txDecoder))
+
+
+txDecoder : Decoder Tx
+txDecoder =
+    (field "code" string)
+        |> andThen txDecode
+
+
+cashInTxDecoder : Decoder CashInTxRec
+cashInTxDecoder =
+    decode CashInTxRec
+        |> required "id" string
+        |> required "machineName" string
+        |> required "toAddress" string
+        |> required "cryptoAtoms" int
+        |> required "cryptoCode" string
+        |> required "fiat" float
+        |> required "currencyCode" string
+        |> required "txHash" (nullable string)
+        |> required "phone" (nullable string)
+        |> required "error" (nullable string)
+        |> required "created" date
+
+
+cashOutTxDecoder : Decoder CashOutTxRec
+cashOutTxDecoder =
+    decode CashOutTxRec
+        |> required "id" string
+        |> required "machineName" string
+        |> required "toAddress" string
+        |> required "cryptoAtoms" int
+        |> required "cryptoCode" string
+        |> required "fiat" float
+        |> required "currencyCode" string
+        |> required "txHash" (nullable string)
+        |> required "status" string
+        |> required "dispensed" bool
+        |> required "notified" bool
+        |> required "redeemed" bool
+        |> required "phone" (nullable string)
+        |> required "error" (nullable string)
+        |> required "created" date
+        |> required "confirmed" bool
---
+++ src/TransactionTypes.elm
@@ -0,0 +1,43 @@
+module TransactionTypes exposing (..)
+
+import Date exposing (Date)
+
+
+type alias CashInTxRec =
+    { id : String
+    , machineName : String
+    , toAddress : String
+    , cryptoAtoms : Int
+    , cryptoCode : String
+    , fiat : Float
+    , currencyCode : String
+    , txHash : Maybe String
+    , phone : Maybe String
+    , error : Maybe String
+    , created : Date
+    }
+
+
+type alias CashOutTxRec =
+    { id : String
+    , machineName : String
+    , toAddress : String
+    , cryptoAtoms : Int
+    , cryptoCode : String
+    , fiat : Float
+    , currencyCode : String
+    , txHash : Maybe String
+    , status : String
+    , dispensed : Bool
+    , notified : Bool
+    , redeemed : Bool
+    , phone : Maybe String
+    , error : Maybe String
+    , created : Date
+    , confirmed : Bool
+    }
+
+
+type Tx
+    = CashInTx CashInTxRec
+    | CashOutTx CashOutTxRec
